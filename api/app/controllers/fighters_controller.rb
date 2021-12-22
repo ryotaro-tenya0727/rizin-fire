97ba73804925c2723eaa08af1bac282c3e5af5bc
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-12-22 15:03:36 +0900

    バックエンド実装

--- api/app/controllers/fighters_controller.rb
+++ api/app/controllers/fighters_controller.rb
@@ -1,7 +1,17 @@
 class FightersController < ApplicationController
+
   def index
+    fighters = Fighter.all
+    render json: {
+      fighters: fighters
+    }, status: :ok
   end
 
   def create
+    @fighter = Fighter.find_by(fighter_number: params[:fighter_number])
+    if @fighter
+      @fighter.update(count: @fighter.count + 1)
+
+    end
   end
 end
