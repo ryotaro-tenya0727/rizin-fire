c98e1eaab9c719969bac364d223a0bc05dac437b
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-12-28 11:22:08 +0900

    【修正】格闘家のボタンのコメント修正

--- front/reactapp/src/containers/Fighters.jsx
+++ front/reactapp/src/containers/Fighters.jsx
@@ -5,7 +5,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 import { HeaderAndResult } from '../styledcomponent/HeaderAndResult';
 
-//格闘家の一つ一つのカード
+//格闘家の枠
 import { FighterWrapper } from './../components/FighterWrapper';
 
 import { ResultButton } from '../Button/ResultButton';
