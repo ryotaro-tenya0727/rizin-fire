ead37e809c9db8384c46a1a823df26c0e714efed
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-02-12 13:36:21 +0900

    test

--- front/reactapp/src/containers/Orders.jsx
+++ front/reactapp/src/containers/Orders.jsx
@@ -1,7 +1,14 @@
 import React, { Fragment } from 'react';
 
 const Orders = () => {
-  return <Fragment>注文画面</Fragment>;
+  return (
+    <Fragment>
+      <div>aa</div>
+      注文画面
+      <div>aaa</div>
+      <div>aaa</div>
+    </Fragment>
+  );
 };
 
 export default Orders;
